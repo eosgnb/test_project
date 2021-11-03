dac651f64233b30fe1cc52928add319803c81ee1
punkznotded <eosuganob@iskolarngbayan.pup.edu.ph>
2021-11-03 13:25:22 +0800

    add sum function

--- test1.c
+++ test1.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+int get_square(int num)
+{
+    return num * num;
+}
+
 int main(void)
 {
-    printf("goodbye, world.\n");
+    int num;
+
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    int square = get_square(num);
+    printf("The square of %d is %d.\n", num, square);
 }
